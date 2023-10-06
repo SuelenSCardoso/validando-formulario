a6a09223c2f03dda8e2cfd4fad0cdcc31a55da51
Suelen Cardoso <sudavj@gmail.com>
2023-10-06 19:46:00 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+const bntEnviar = document.getElementById('bnt-enviar');
+const caixaTexto = document.querySelectorAll('.input-text');
+const span = document.querySelectorAll('.campo-obrigatorio');
+
+
+bntEnviar.addEventListener('click', () => {
+     caixaTexto.forEach(function (input, indice)  {     
+         if (input.value === "") {
+             input.classList.remove("campo-preenchido");
+             input.classList.add("n-preenchido");
+             span[indice].classList.add("erro");
+         } else {
+             input.classList.add("campo-preenchido");
+             input.classList.remove("n-preenchido");
+             span[indice].classList.remove("erro");
+         }
+     })   
+ })               
